227f5141461b4c857ce9983b8ed0b8c56b6d4705
Hannah <hmansoori97@gmail.com>
2020-01-11 10:44:19 -0800

    initial commit

---
+++ pokedex/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import SideDrawer from './components/SideDrawer';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <SideDrawer />
+    </div>
+  );
+}
+
+export default App;
