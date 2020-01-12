aaf161b48439bc852c308db82ecd1a7617d47b1d
Hannah <hmansoori97@gmail.com>
2020-01-11 18:03:36 -0800

    add buildUrl and searchPokemon functions

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var baseUrl = "https://pokeapi.co/api/v2/";
+
+function buildUrl(call, input) {
+    //categories [berries, contests, encounters, evolution, games, items, locations, machines, moves, pokemon]
+    var requestUrl = baseUrl + call + '/' + input;
+    return requestUrl;
+}
+
+function searchPokemon(url) {
+    fetch(url).then(res => res.json())
+    .then((result) => {
+        return result;
+    },
+    (error) => {
+        console.error(error);
+        return error;
+    })
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
